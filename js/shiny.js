026227e686064fffb1686ccd64d02baf8689ea14
Rob Drimmie <rob.drimmie@gmail.com>
2011-11-12 09:15:58 -0500

    Added jQuery to do interesting events lazily. checking the variables I added last time and when appropriate triggering an event. still nothing happens in an event, going to place with a few things to try neato stuff.

--- js/shiny.js
+++ js/shiny.js
@@ -1,33 +1,48 @@
 var shine = {
-    totalInteractions: 0,
-    interactionsSinceLastEvent: 0,
-    nextEventAt: 0,
+  totalInteractions: 0,
+  interactionsSinceLastEvent: 0,
+  nextEventAt: 0,
 
-    random256: function() {
-      return Math.floor( Math.random() * 256 );
+  random256: function() {
+    return Math.floor( Math.random() * 256 );
+  },
+
+  background: {
+    red: 0,
+    green: 0,
+    blue: 0,
+
+    toHex: function() {
+      return "#" + shine.background.red.toString(16) +
+                   shine.background.green.toString(16) +
+                   shine.background.blue.toString(16);
     },
 
-    background: {
-      red: 0,
-      green: 0,
-      blue: 0,
-      toHex: function() {
-        return "#" + shine.background.red.toString(16) +
-                     shine.background.green.toString(16) +
-                     shine.background.blue.toString(16);
-      },
-      randomize: function() {
-        shine.background.red = shine.random256();
-        shine.background.green = shine.random256();
-        shine.background.blue = shine.random256();
-      }
-    },
-
-    on: function() {
-      var body = document.getElementsByTagName( 'body' )[0];
-          shine.background.randomize();
-          body.style.backgroundColor = shine.background.toHex();
+    randomize: function() {
+      shine.background.red = shine.random256();
+      shine.background.green = shine.random256();
+      shine.background.blue = shine.random256();
     }
+  },
+
+  doEvent: function() {
+
+  },
+
+  on: function() {
+    shine.totalInteractions++;
+    shine.interactionsSinceLastEvent++;
+
+    if( shine.interactionsSinceLastEvent >= shine.nextEventAt ) {
+      shine.doEvent();
+      shine.interactionsSinceLastEvent = 0;
+      shine.nextEventAt = Math.floor( Math.random() * 100 );
+    }
+    var body = document.getElementsByTagName( 'body' )[0];
+
+    shine.background.randomize();
+    body.style.backgroundColor = shine.background.toHex();
+  }
 }
 
 window.addEventListener( 'load', function() {
