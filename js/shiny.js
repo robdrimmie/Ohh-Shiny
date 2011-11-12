b885432aa0ff5c38589b5979ea5a074ac3983d9e
Rob Drimmie <rob.drimmie@gmail.com>
2011-11-11 19:41:33 -0500

    Mostly restructuring the existing logic, bulking it up a bit with some object-y context but overall largely better. Added some seeds of my thoughts for random events happening at certain points but need to take advantage of those variables.

--- js/shiny.js
+++ js/shiny.js
@@ -1,13 +1,36 @@
-var shine = function() {
-  var red = Math.floor( Math.random() * 256 ),
-      green = Math.floor( Math.random() * 256 ),
-      blue = Math.floor( Math.random() * 256 );
+var shine = {
+    totalInteractions: 0,
+    interactionsSinceLastEvent: 0,
+    nextEventAt: 0,
 
-  document.getElementsByTagName( 'body' )[0].style.backgroundColor = "#" + red.toString(16) + green.toString(16) + blue.toString(16);
-  console.log( 'c' );
+    random256: function() {
+      return Math.floor( Math.random() * 256 );
+    },
+
+    background: {
+      red: 0,
+      green: 0,
+      blue: 0,
+      toHex: function() {
+        return "#" + shine.background.red.toString(16) +
+                     shine.background.green.toString(16) +
+                     shine.background.blue.toString(16);
+      },
+      randomize: function() {
+        shine.background.red = shine.random256();
+        shine.background.green = shine.random256();
+        shine.background.blue = shine.random256();
+      }
+    },
+
+    on: function() {
+      var body = document.getElementsByTagName( 'body' )[0];
+          shine.background.randomize();
+          body.style.backgroundColor = shine.background.toHex();
+    }
 }
 
 window.addEventListener( 'load', function() {
-  document.addEventListener( 'click', shine );
-  document.addEventListener( 'keyup', shine );
+  document.addEventListener( 'click', shine.on );
+  document.addEventListener( 'keypress', shine.on );
 });
